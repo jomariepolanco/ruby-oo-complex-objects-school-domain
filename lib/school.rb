cb3e24cdd364682dca453410a1f395d1900a3ab9
jomariepolanco <jomariepolanco@gmail.com>
2020-10-06 18:12:54 -0400

    domain-model lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name 
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        @grade = grade 
+        @student_name = student_name 
+
+        if @roster.keys.include?(@grade)
+            @roster[@grade] << @student_name 
+        else
+            @roster[@grade] = []
+            @roster[@grade] << @student_name  
+        end 
+    end 
+
+    def grade(grade)
+        @grade = grade 
+        @roster[@grade] 
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end
+    end
+end
